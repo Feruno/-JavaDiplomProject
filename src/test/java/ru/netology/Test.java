668accfdd12958d9ba148a87bce1600bfe23c61f
Александр Бондаренко <nice.alexsandr@mail.ru>
2023-12-10 21:11:26 +0300

    first commit

---
+++ src/test/java/ru/netology/Test.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Test {
+
+}
